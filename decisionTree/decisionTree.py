fca322b141e5062b160afdeac380490db20bfe21
rechar <884520248@qq.com>
2020-11-20 22:24:45 +0800

    decision 完成

--- decisionTree/decisionTree.py
+++ decisionTree/decisionTree.py
@@ -39,14 +39,14 @@ class ID3(object):
         self.data = pd.DataFrame(dataC, columns=feature)
         return self.data
 
-    def get_Ent(data):
+    def get_Ent(self, data):
 
         num_data = len(data)
         countClass = {}
 
         for i in range(num_data):
-            new_data = data[i]
-            label = data['labels']
+            new_data = data.iloc[i, :]
+            label = new_data['labels']
 
             if label not in countClass.keys():
                 countClass[label] = 0
@@ -60,7 +60,7 @@ class ID3(object):
         return Ent
 
 
-    def get_gain(data, origin_Ent, feature):
+    def get_gain(self, data, origin_Ent, feature):
 
         featureValue = data[feature]
         unique_featureValue = set(featureValue)
@@ -74,7 +74,7 @@ class ID3(object):
         return origin_Ent - Ent
 
 
-    def choose_best(data):
+    def choose_best(self, data):
         
         num_feature = len(data.columns) - 1 #特征的数量
         origin_Ent = self.get_Ent(data)
@@ -91,17 +91,62 @@ class ID3(object):
         return best_feature
 
 
+    def create_tree(self, data):
+        
+        feature = data.columns[:-1].tolist()
+        label = data.iloc[:, -1]
+        
+        #完全归为一类
+        if len(data['labels'].value_counts()) == 1:
+            node = data['labels'].mode().values
+            return node
 
+        #如果说类别分完
+        if len(feature) == 1:
+            node = data['labels'].mode().values
+            return node
+        
 
+        #选择一个最优特征作为根
+        best_feature = self.choose_best(data)
 
+        tree = {best_feature:{}}
+
+        best_feature_value = data[best_feature]
+
+        unique_value = set(best_feature_value)
+
+        for each in unique_value:
+            datax = data[data[best_feature] == each]
+            datax = datax.drop([best_feature], axis=1)
+            tree[best_feature][each] = self.create_tree(datax)
+
+        return tree
+
+    def predict(self, tree, test):
+        first_feature = list(tree.keys())[0]  # 获取根节点
+        feature_dict = tree[first_feature]  # 根节点下的树
+        labels = test.columns.tolist()
+        value = test[first_feature][0]
+        for key in feature_dict.keys():
+            if value == key:
+                if type(feature_dict[key]).__name__ == 'dict':  # 判断该节点是否为叶节点
+                    class_label = self.predict(feature_dict[key], test)  # 采用递归直到遍历到叶节点
+                else:
+                    class_label = feature_dict[key]
+        return class_label
     
-
-
-    
-
 
 
 if __name__ == "__main__":
     
     id3 = ID3()
     data = id3.CreateData()
+    #print(data.iloc[1])
+    tree = id3.create_tree(data)
+    #print(tree)
+    test = pd.DataFrame({"hair": ["long"], "voice": ["thin"], "ear_stud": ["yes"]})
+    pred = id3.predict(tree, test)
+    print(pred)
+
+
